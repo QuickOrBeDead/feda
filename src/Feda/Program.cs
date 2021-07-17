3b0241aab2e5255ec4eba788e4c4e0f970e164f9
QuickOrBeDead <boraakgn@gmail.com>
2021-07-17 15:02:57 +0300

    test git config

--- src/Feda/Program.cs
+++ src/Feda/Program.cs
@@ -8,21 +8,7 @@
         {
             Console.WriteLine("Feda!");
 
-            var b = 0UL;
-
-            for (var rank = 0; rank < 8; rank++)
-            {
-                for (var file = 0; file < 8; file++)
-                {
-                    var square = (rank * 8) + file;
-                    if (file < 6)
-                    {
-                        Bit.SetBit(ref b, square);
-                    }
-                }
-            }
-
-            Board.PrintBoard(b);
+            Board.PrintBoard(0UL);
         }
     }
 }
